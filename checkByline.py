37dea485fcc6b07372bd762c82a42969174d6dc9
David Spearman <107007240+DESpear262@users.noreply.github.com>
2023-06-02 17:23:58 -0400

    Update checkByline.py

--- checkByline.py
+++ checkByline.py
@@ -1,5 +1,5 @@
 def checkByline(file):
     with open(file, encoding="utf8") as f:
         for line in f:
-            if '<!-- <a href="https://www.econlib.org/' in line:
-                return 'author/bcaplan/" class="author url fn" rel="author"> -->Bryan Caplan' in line
+            if '<!-- <a href="https://www.econlib.org/' in line:                                            #Unique identifier for byline code. Also appears in recommended articles but main article byline always appears first
+                return 'author/bcaplan/" class="author url fn" rel="author"> -->Bryan Caplan' in line       #Unique identifier for Bryan's articles in byline code line
